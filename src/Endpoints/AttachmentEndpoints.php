3f9b1625a05b2dc8e75ae0795893fc0256c8e042
David Good <dgood@gdba.com>
2021-06-24 12:47:58 -0500

    Adding Attachment Data

--- src/Endpoints/AttachmentEndpoints.php
+++ src/Endpoints/AttachmentEndpoints.php
@@ -12,18 +12,17 @@ trait AttachmentEndpoints
         }
     }
     
-    public function getRowAttachments($sheet_id, $row_id)
+    public function getRowAttachments($sheet_id, $row_id, $page = 1, $pageSize = 100)
     {
-        $url = $this->url . self::SHEETS . "$sheet_id/rows/$row_id/attachments";
+        $url = $this->url . self::SHEETS . "$sheet_id/rows/$row_id/attachments?page$page&pageSize=$pageSize";
         try {
             $results = json_decode($this->client()->get($url)->getBody()->getContents());
-            dd($results);
             return $this->respond(200, 'Success.', [
-                'name' => $results->name,
-                'version' => $results->version,
-                'rowCount' => $results->totalRowCount,
-                'columns' => $results->columns,
-                'rows' => $results->rows
+                'pageNumber' => $results->pageNumber,
+                'pageSize' => $results->pageSize,
+                'totalPages' => $results->totalPages,
+                'totalCount' => $results->totalCount,
+                'data' => $results->data
             ]);
         } catch (\Exception $e) {
             return $this->respond(500, 'Unable to get sheet version.', ['exception' => $e->getMessage()]);
